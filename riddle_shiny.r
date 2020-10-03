04dcc509bee2ecfdc1a108fe7fe88d7e7e849ec2
Joris van Zundert <joris.van.zundert@gmail.com>
2020-10-04 00:02:31 +0200

    Exchanged pie chart for waffle chart.

--- riddle_shiny.r
+++ riddle_shiny.r
@@ -1,13 +1,15 @@
 library( shiny )
 library( litRiddle )
-library(RColorBrewer)
+library( RColorBrewer )
+library( ggplot2 )
+library( waffle )
 
 options( shiny.port = 4800 )
 
 data( respondents )
 
-myPalette <- rev( brewer.pal( 5, "Pastel2" ) )
-rev( myPalette )
+waffle_palette <- rev( brewer.pal( 5, "Pastel2" ) )
+bar_palette <- brewer.pal( 5, "Greens" )
 
 ui <- fluidPage(
   titlePanel( "title panel" ),
@@ -31,18 +33,21 @@ server <- function( input, output ){
   hist_plot <- reactive({
     title <- paste( 'Verdeling van het aantal boeken per jaar binnen\nde groep van', input$age_range[1], 'tot', input$age_range[2], 'jaar' )
     return( hist( respondents[ respondents[ 'age.resp' ] >= input$age_range[1] & respondents[ 'age.resp' ] <= input$age_range[2], 'books.per.year' ], breaks=seq( 0, 700, by=25 ),
-      main=title, xlim=c( 0, 400 ), ylim=c( 0, 9000 ), xlab='Boeken per jaar', ylab='Aantal respondenten', cex=1.2, cex.lab=1.2, cex.main=1.5, col=myPalette ) )
+      main=title, xlim=c( 0, 400 ), ylim=c( 0, 9000 ), xlab='Boeken per jaar', ylab='Aantal respondenten', cex=1.2, cex.lab=1.2, cex.main=1.5, col=bar_palette ) )
   })
-  pie_plot <- reactive({
+  waffle_plot <- reactive({
     total_books_per_year <- sum( na.omit( respondents[ 'books.per.year' ] ) )
-    total_books_per_year
     books_per_year_at_age <- sum( na.omit( respondents[ respondents[ 'age.resp' ] >= input$age_range[1] & respondents[ 'age.resp' ] <= input$age_range[2], 'books.per.year' ] ) )
-    slices <- c( total_books_per_year - books_per_year_at_age, books_per_year_at_age )
-    lbls <- c( 'Anderen', paste( input$age_range[1], 'tot', input$age_range[2] ,'jaar' ) )
-    return( pie( slices, labels=lbls, main=paste( 'Totaal aantal boeken per jaar gelezen door\nmensen in de leeftijdsgroep van', input$age_range[1], 'tot', input$age_range[2], 'jaar' ),
-      cex=1.2, cex.main=1.5, col=myPalette ) )
+    vals <- c( books_per_year_at_age, total_books_per_year - books_per_year_at_age )
+    vals <- round( vals/2000 )
+    names( vals ) <- sprintf( '%s', scales::percent( round( vals/sum( vals ), 2 ) ) )
+   # return( pie( slices, labels=lbls, main=paste( 'Totaal aantal boeken per jaar gelezen door\nmensen in de leeftijdsgroep van', input$age_range[1], 'tot', input$age_range[2], 'jaar' ),
+   #    cex=1.2, cex.main=1.5, col=myPalette ) )
+    waffle_title <- paste( 'Boeken gelezen per jaar door mensen\nin de leeftijdsgroep van', input$age_range[1], 'tot', input$age_range[2], 'jaar\nten opzichte van het totaal van alle\ngelezen boeken per jaar' )
+    return( waffle( vals, size=1, colors=c( waffle_palette[1:2], 'white' ),
+      title=waffle_title ) + theme( legend.position='bottom' ) )
   })
-  output$plotgraph1 = renderPlot( pie_plot() )
+  output$plotgraph1 = renderPlot( waffle_plot() )
   output$plotgraph2 = renderPlot( hist_plot() )
 }
 
